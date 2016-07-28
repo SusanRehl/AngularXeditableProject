69a395749905c65972884f3e92dfe07aa058b6ae
Susan Rehl <susanrehl@gmail.com>
2016-07-27 20:16:06 -0500

    Initial Commit

--- public/scripts.js
+++ public/scripts.js
@@ -1,86 +1,67 @@
-$( document ).ready(function(){  // sets document for jQuery
-  showList();  // calls showlist function to get tasks from database and send to DOM
+var myApp=angular.module('myApp', ["xeditable"]);
 
-  $('#addButton').on('click', function(){  // get task name from input
-    var taskName = $('#taskIn').val();
-    var taskStatus = false;  // sets taskstatus to false (not completed)
-    var newTask = {  // creates object to send
-      "taskname": taskName,
-      "taskstatus": taskStatus,
-    }; // end object
-    $.ajax({    //send new task to database
-      type: 'POST',
-      url: '/addTask',
-      data: newTask,
-      success: function(data){
-            showList();  // calling showList function to refresh DOM
-      }
-    }); //end ajax
-    clearInput(); // calls clearInput function to clear value of text input box
-  }); //end button on click function
+myApp.run(function(editableOptions) {
+  editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2', 'default'
+});
 
-  function clearInput() {  // clears data from input box
-      document.getElementById('taskIn').value='';
-    }   // end of clearinput box
+myApp.controller('Ctrl', function($scope, $filter, $http) {
 
-function showList(){ // get to do list from database and appends to outputDiv
-    $.ajax({
-      type: 'GET',
-      url: '/getList',
-      success: function(dataIn){
-        $('#outputDiv').empty();  // resets div to empty
-        for(i=0; i<dataIn.length; i++) {  //loop thru database
-          var completedButton = "";
-          var taskOut = "";
-          var deleteButton = "<button class='delete' data-id='" + dataIn[i].id + "'>&#10006;" + "</button>";
-          if (dataIn[i].taskstatus === true) {  // adds class done and disables to completed tasks
-            completedButton = "<button class='complete done' disabled data-id='" + dataIn[i].id + "'>&#10003;" + "</button>";
-            taskOut = "<p class='doneTask'>" + dataIn[i].taskname;  // adds class doneTask to taskname
-            } else {
-            completedButton = "<button class='complete' data-id='" + dataIn[i].id + "'>&#10003;" + "</button>";
-            taskOut = "<p>" + dataIn[i].taskname;
-            }
-          $('#outputDiv').append(completedButton);  // sends all to DOM
-          $('#outputDiv').append(deleteButton);
-          $('#outputDiv').append(taskOut);
-        }
-      } // end success
-    }); //end ajax call to get list
-  }// end show list function
+  $scope.outsideArray=[];
+  var objectToSend = {};
 
-  $('body').on('click', '.delete', function() {  // delete task function
-    var delClick = confirm('Are you sure?');  // alert box if completed box is checked
-      if(delClick === true) {
-    $('#outputDiv').empty();  // if yes, then proceeds with function
-    var taskID = {
-      "id": $(this).data('id')
-    };
-    $.ajax({  // sends to delete from database
-      type: 'POST',
-      url: '/deleteTask',
-      data: taskID,
-      success: function(data) {
-        showList(data);
-      }
+  $scope.getRoom = function() {
+    console.log("in getRoom function in script");
+    roomToSend = {
+         room_number: $scope.roomnum
+     };
+     console.log(roomToSend);
+    $http({   // gets recordset via POST
+      method: 'POST',
+      url: '/getRoom',
+      data: roomToSend
+    }).then(function() {
+      $scope.showRoom();
     });
-  } else {  // if no, stops function
-  }
-  });
-  $('body').on('click', '.complete', function() {  // complete task function
-    // $('#outputDiv').empty();
-    console.log($(this).data('id'));
-    $('#outputDiv').empty();
-    var taskID = {
-      "id": $(this).data('id')
-    };
-    $.ajax({  // sends to delete from database
-      type: 'POST',
-      url: '/statusComplete',
-      data: taskID,
-      success: function(data) {
-        showList(data);
-      }
-    });
-  });
+  }; // end getRoom function
 
-}); // end document ready
+ $scope.roomToShow = [];
+
+$scope.showRoom = function() {
+  console.log("in show room function in script");
+  $http({   // gets recordset via GET
+    method: 'GET',
+    url: '/showRoom',
+  }).then( function(response){  // success call - runs function with response parameter
+  // console.log(response.data);
+    $scope.roomToShow = response.data;  // pulls the data from app.js and sets to global var roomToShow
+    console.log($scope.roomToShow);
+  }, function myError(response){
+    console.log(response.statusText);
+  }// end error function
+  ); // end then response
+  $scope.roomnum="";
+}; // end showRoom function
+
+$scope.makeSqlHappy=function(recordroom_number, recordroom_type, recordcapacity, recordprice, recordcheck_in_date, recordcheck_out_date, recordnotes, recordid) {
+  console.log('in makeSqlHappy');
+  console.log("data from makeSqlHappy: ", recordroom_number, recordroom_type, recordcapacity, recordprice, recordcheck_in_date, recordcheck_out_date, recordnotes);
+  console.log("makeSqlHappy's stinkin id: ", recordid);
+  var id=recordid;
+  var data={
+    room_number: recordroom_number,
+    room_type: recordroom_type,
+    capacity: recordcapacity,
+    price: recordprice,
+    check_in_date: recordcheck_in_date,
+    check_out_date: recordcheck_out_date,
+    notes: recordnotes
+  };
+  console.log(data);
+  $http({
+    method: 'PUT',
+    url: '/saveRoom/' +id,
+    data: data
+  });
+};
+
+}); //end controller
--- server/app.js
+++ server/app.js
@@ -2,78 +2,80 @@ var express=require('express');  // sets up express
 var app=express();
 var path=require('path');  // sets up paths
 var bodyParser=require('body-parser');  // sets up body-parser for POST method
-var urlencodedParser=bodyParser.urlencoded( {extended: false} );
 var pg=require('pg');  // sets up postgres database
-var connectionString='postgres://localhost:5432/todolist';
+var connectionString='postgres://localhost:5432/angularxeditable';  /// put database name in
 
-app.get('/', function(req, res) {  // sets up base url
-  console.log('hello from base url get');
-  res.sendFile(path.resolve('views/index.html')); // sends the index.html file to the browser!!!!!!!!!!!
-});
-
-app.get('/getList', function(req, res) { // displaying to do list - uses GET
-  console.log("in app.get get to do list");
-  var results = [];  // array to hold tasks
-  pg.connect(connectionString, function(err, client, done) {  // connecting to todolist database
-    var todoList=client.query('SELECT * FROM tdlist ORDER BY taskstatus;');  // getting task and status from tdlist table
-    console.log('query '+ todoList);
-    var rows = 0;
-    todoList.on('row', function(row) {  // pushing to array
-      results.push(row);
-    });  // end query push
-    todoList.on('end', function() {  // sending to scripts
-      return res.json(results);
-    });
-    done(); // allowing more than 5 tasks to be added
-  });
-  });
-
-app.post('/addTask',urlencodedParser, function(req, res) {  // adding task to database and DOM
-  var newTask = req.body.taskname + req.body.taskstatus;
-  var results = [];  // array to hold tasks
-  pg.connect(connectionString, function(err, client, done) {
-    client.query('INSERT INTO tdlist(taskname, taskstatus) VALUES($1, $2)', [req.body.taskname, req.body.taskstatus]);
-      res.send(newTask);
-      done();
-  }); // end database connection
- }); // end add product post function
-
-app.post('/deleteTask', urlencodedParser, function(req, res) {  // deleting task from database and DOM
-    var results =[];
-    pg.connect(connectionString, function(err, client, done) {
-      var resetTasks = client.query('DELETE FROM tdlist WHERE id = ' + req.body.id + ';');
-      console.log( "query: " + resetTasks );
-      var rows = 0;  // push each row in query into results array
-      resetTasks.on( 'row', function ( row ){
-        results.push( row );
-        res.send(results);
-        }); // end query push
-      resetTasks.on( 'end', function (){  // return results array
-        return res.json(results);
-        });
-        done();
-    });
-});
-
-app.post('/statusComplete', urlencodedParser, function(req, res) { // changes status of completed tasks
-    var results =[];
-    pg.connect(connectionString, function(err, client, done) {
-      var resetTasks = client.query('UPDATE tdlist set taskstatus=true WHERE id= ' + req.body.id + ';');
-      console.log( "query: " + resetTasks );
-      var rows = 0;
-      resetTasks.on( 'row', function ( row ){  // push each row in query into results array
-        results.push( row );
-        res.send(results);
-        }); // end query push
-      resetTasks.on( 'end', function (){  // sends results array to DOM
-        return res.json(results);
-        });
-        done();
-    });
-});
+app.use(bodyParser.json());
+app.use(express.static('public')); // makes public folder available
 
 app.listen(3000, 'localhost', function(req, res) {  // spin up port
   console.log("listening on port 3000");
 });
 
-app.use(express.static('public')); // makes public folder available
+app.get('/', function(req, res) {  // sets up base url
+  console.log('hello from base url get' + path.resolve('views/index.html'));
+  res.sendFile(path.resolve('views/index.html')); // sends the index.html file to the browser!!!!!!!!!!!
+});
+
+var selectedRoom=[];
+
+app.post('/getRoom', function(req, res) { // pulling selected room info from database to display on room picker
+    console.log("in app.post  getroom");
+    console.log(req.body);
+    selectedRoom = [];  // resets array to empty for new room
+    pg.connect(connectionString, function(err, client, done) {  // connecting to disinfectants database
+      if (err) {     // check for errors
+      console.log(err);
+    } else { // start selection criteria
+        //  roomInfo=client.query("SELECT * FROM roominfo WHERE room_number= ' + req.body.room_number ' ");
+         roomInfo=client.query("SELECT * FROM roominfo WHERE room_number= '" + req.body.room_number +"'");
+         console.log("roominfoin app: ");
+          rows = 0;
+          roomInfo.on('row', function(row) {  // pushing to array
+            selectedRoom.push(row);
+          });  // end query push
+          roomInfo.on('end', function() {  // sending to scripts
+            console.log("room info from app.post in app", selectedRoom);
+            return res.json(selectedRoom);
+          }); // end products.on function
+          done(); // signals done
+      } // end else (for success)
+    }); // end pg connect function
+}); // end app.post /getRoomInfo function
+
+app.get( '/showRoom', function( req, res ){  // makes returned room info available to room assigner
+      console.log("in showRoom function in app: ", selectedRoom);
+      return res.json(selectedRoom);
+  }); // end app.get /showRoom function
+
+app.put('/saveRoom/:id', function(req, res) {
+  var room = req.body;
+  var id = req.params.id;
+  pg.connect(connectionString, function(err, client, done) {
+    if (err){  //connection error
+      console.log('error at pg connect.');
+      res.sendStatus(500);
+    } // end connection error
+    console.log("in client.query update");
+    client.query('UPDATE roominfo ' +
+      'SET room_type = $1, ' +
+      'capacity = $2, ' +
+      'room_number = $3, ' +
+      'notes = $4, ' +
+      'price = $5, ' +
+      'check_in_date = $6, ' +
+      'check_out_date = $7 ' +
+      'WHERE id = $8',
+       [room.room_type, room.capacity, room.room_number, room.notes, room.price, room.check_in_date, room.check_out_date, id],  // updating room info changes
+    function(err, result){
+      done();
+      if (err) {
+        console.log("error in app.js:");
+         console.log(err);
+          res.sendStatus(500);
+          return;
+      }
+      res.sendStatus(204);  //PUT success
+    });
+  }); //end connection
+}); //end app.put function

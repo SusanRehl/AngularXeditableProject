ad75cfbc7cfdb9d082735a6d7256e5a8d16a0738
Susan Rehl <susanrehl@gmail.com>
2016-08-11 11:46:59 -0500

    updated database.sql file and added image of db structure

--- database.sql
+++ database.sql
@@ -1,6 +1,12 @@
-CREATE TABLE tdlist (
-	id SERIAL PRIMARY KEY NOT NULL,
-	taskname VARCHAR(50),
-	taskstatus BOOLEAN
+CREATE TABLE roominfo (
+	id SERIAL PRIMARY KEY NOT NULL DEFAULT next value for roominfo_id_seq,
+	room_type VARCHAR(20),
+	capacity INTEGER,
+	room_number VARCHAR(20),
+	notes VARCHAR (30),
+	price INTEGER,
+	check_in_date VARCHAR(20),
+	check_out_date VARCHAR(20)
 	);
-	
+
+CREATE UNIQUE INDEX roominfo_pkey ON roominfo USING btree (id);
